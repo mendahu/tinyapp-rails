86ea46a2541a9065adf9ac4bc8b2493b1e7be0d8
Jake Robins <me@jakerobins.com>
2021-11-25 20:27:25 -0600

    add urls/show route and partial header

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -2,4 +2,9 @@ class UrlsController < ApplicationController
   def index
     @urls = Url.all
   end
+
+  def show
+    short_url = params[:short_url]
+    @url = Url.find_by(short_url: short_url)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/urls", to: "urls#index"
+  get "/urls/:short_url", to: "urls#show"
 end
