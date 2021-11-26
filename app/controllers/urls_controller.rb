a636187825b301690eb4b00e9e7ae5c8ed1dbed9
Jake Robins <me@jakerobins.com>
2021-11-25 21:32:30 -0600

    add create route

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,3 +1,5 @@
+require 'securerandom'
+
 class UrlsController < ApplicationController
   def index
     @urls = Url.all
@@ -8,5 +10,19 @@ class UrlsController < ApplicationController
   end
 
   def new
+    @url = Url.new
+  end
+
+
+  def create
+    @url = Url.new
+    @url.short_url = SecureRandom.hex(3)
+    @url.long_url = params[:url][:long_url]
+
+    if @url.save
+      redirect_to @url
+    else
+      render :new
+    end
   end
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,5 +1,7 @@
 class Url < ApplicationRecord
   belongs_to :user
+  validates :short_url, presence: true
+  validates :long_url, presence: true
   def to_param
     short_url
   end
